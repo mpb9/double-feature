f6dfddf28d6608372f3ccf06397ebf6a9bc981e0
mpb9 <michaelbeebe1031@gmail.com>
2025-05-09 18:40:28 -0400

    omg docker works

---
+++ cli/build/client/assets/escapism-1doxgMly.js
@@ -0,0 +1,1 @@
+import{w as e}from"./with-props-CTzvD32q.js";import{o as s}from"./chunk-D4RADZKF-8YUgi0Se.js";import{A as i}from"./article-header-Y4hjm6_U.js";import"./index-CoLcMgoR.js";const c=e(function({loaderData:t}){const r=t;return s.jsxs(s.Fragment,{children:[s.jsx(i,{fileSpecs:r}),s.jsx("main",{children:s.jsx("h2",{children:r.title})})]})});export{c as default};
