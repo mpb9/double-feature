85bc22a8b273881625d1ba037e15d3e882fbf07a
mpb9 <michaelbeebe1031@gmail.com>
2025-05-09 05:01:22 -0400

    Update .dockerignore, Dockerfile, and archive data; add new images for film archive

--- cli/app/components/home/home-link.jsx
+++ cli/app/components/home/home-link.jsx
@@ -2,14 +2,13 @@ import { Link } from "react-router";
 export default function HomeLink({ name, url }) {
   if (name == "Home" || name == "") {
     return (
-      <div className="flex flex-col mx-6">
+      <div className="flex flex-col mx-4">
         <div className="flex flex-row items-center justify-between w-full text-2xl font-[200] tracking-widest">
           <Link
             to="/"
             className="text-[var(--dim)] underline underline-offset-8 decoration-[#69d346aa] decoration-2 hover:text-[#69d346] duration-300"
           >
-            {" "}
-            HOME{" "}
+            HOME
           </Link>
         </div>
       </div>
--- cli/app/data/archive-data.js
+++ cli/app/data/archive-data.js
@@ -12,7 +12,7 @@ export const ARCHIVE_FILES = [
   new FileSpecs(
     "2023",
     "2023",
-    "2023",
+    "Past_Lives-2023.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2023/",
     ["lists", "year"],
@@ -21,23 +21,23 @@ export const ARCHIVE_FILES = [
   new FileSpecs(
     "2022",
     "2022",
-    "2022",
+    "Aftersun-2022.png",
     "",
     "https://letterboxd.com/michaelbeebe/list/2022/",
     ["lists", "year"],
     "2022-12-31"
   ),
   new FileSpecs(
-    "",
     "2021",
     "2021",
+    "Dune_2021.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2021/",
     ["lists", "year"],
     "2021-12-31"
   ),
   new FileSpecs(
-    "",
+    "2020",
     "2020",
     "Tenet_005.jpg",
     "",
@@ -46,9 +46,9 @@ export const ARCHIVE_FILES = [
     "2020-12-31"
   ),
   new FileSpecs(
-    "",
     "2019",
     "2019",
+    "Parasite-2019.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2019/",
     ["lists", "year"],
@@ -57,34 +57,34 @@ export const ARCHIVE_FILES = [
   new FileSpecs(
     "",
     "2018",
-    "2018",
+    "TheFavourite-2018.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2018/",
     ["lists", "year"],
     "2018-12-31"
   ),
   new FileSpecs(
-    "",
     "2017",
     "2017",
+    "bladerunner-2049-2017.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2017/",
     ["lists", "year"],
     "2017-12-31"
   ),
   new FileSpecs(
-    "",
     "2016",
     "2016",
+    "The_Handmaiden-2016.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2016/",
     ["lists", "year"],
     "2016-12-31"
   ),
   new FileSpecs(
-    "",
     "2015",
     "2015",
+    "MadMax-2015.jpg",
     "",
     "https://letterboxd.com/michaelbeebe/list/2015/",
     ["lists", "year"],
