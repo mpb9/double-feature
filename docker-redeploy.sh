bad786e5fafd5eb186a9c569f1530da16271b289
mpb9 <michaelbeebe1031@gmail.com>
2025-07-18 18:55:41 -0400

    - updated redeploy script to only open the Render frontend page when finished

--- docker-redeploy.sh
+++ docker-redeploy.sh
@@ -14,5 +14,6 @@ docker push michaelbeebe/double-feature-mongo:latest
 docker push michaelbeebe/double-feature-backend:latest
 docker push michaelbeebe/double-feature-frontend:latest
 
-open https://hub.docker.com/repositories/michaelbeebe
-open https://dashboard.render.com/project/prj-d0gfggadbo4c73bbl2og
+# open https://hub.docker.com/repositories/michaelbeebe # Docker Hub repository
+# open https://dashboard.render.com/project/prj-d0gfggadbo4c73bbl2og # Render project
+open https://dashboard.render.com/web/srv-d0gfgsq4d50c73fq6mh0 # Render frontend
